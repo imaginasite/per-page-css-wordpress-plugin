8315e4e282106a96ed57175f51ad74dfd4ce498f
anismk <kouddane@gmail.com>
2026-05-15 23:59:55 +0100

    Remove temporary test file

--- test-meta.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'wp-load.php';
-$pt = get_post_type_object('wp_template');
-echo "wp_template supports custom-fields: " . (post_type_supports('wp_template', 'custom-fields') ? 'yes' : 'no') . "\n";
